3db018e28076e1f71acd9a7099055c782d32f944
lgbin879 <lgbin879@163.com>
2018-10-09 22:47:34 +0800

    download ximalaya audio

---
+++ README.md
@@ -0,0 +1,1 @@
+# download-ximalaya-audio
---
+++ xmlyMp3Dl.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2018/10/08 
+# @Author   : liguibin
+#
+
+###
+'''
+usage: xmlyMp3Dl.py [-h] [-o OUTPUT] [-v] [-n] url
+
+download all audioes in ximalay album like
+:https://www.ximalaya.com/ertong/11106118
+
+positional arguments:
+  url                   web url need to download
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OUTPUT, --output OUTPUT
+                        output file name
+  -v, --verbosity       increase output verbosity
+  -n, --noIndex         not add index to prefix
+'''
+
+import os
+import re
+import sys
+import json
+import requests
+import argparse
+import subprocess
+from bs4 import BeautifulSoup
+
+
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36',
+    }
+
+defaultOutputFile = 'dl_xmly.sh'
+
+urlModel0 = 'https://www.ximalaya.com/'
+urlModel1 = 'http://www.ximalaya.com/'
+defaultAlbumUrl = 'https://www.ximalaya.com/ertong/11106118'
+
+
+#albumId=3533672(aId), pageNum=1(pNum)
+fetchUrlModel = 'https://www.ximalaya.com/revision/play/album?albumId=aId&pageNum=pNum&sort=-1&pageSize=30'
+
+
+def main(albumUrl, outputFile, noIndex):
+    f = open(outputFile, 'w+')
+
+    res = requests.get(albumUrl, headers=headers)
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    # extract number from string
+    albumIdStr = re.findall('\d+', albumUrl)[0]
+    songNum = int(re.findall('\d+', soup.findAll('h2', attrs={'class': 'dOi2'})[0].text)[0])
+
+    if len(soup.findAll('input', attrs={'class': 'Yetd control-input'})):
+        pageNum = int(soup.find_all('input', attrs={'class': 'Yetd control-input'})[0]["max"])
+    else:
+        pageNum = 1
+
+    print("## Info : albumIdStr = %s, pageNum = %d, songNum = %d" %(albumIdStr, pageNum, songNum))
+    
+    for i in range(1, pageNum+1):
+        pageUrl = albumUrl+r'/p'+str(i)
+        fetchUrl = fetchUrlModel.replace('aId', albumIdStr).replace('pNum', str(i))
+        resPage = requests.get(fetchUrl, headers=headers)
+
+        if resPage.status_code == 200:
+            print('\n## Info : Success get %s\n' %(fetchUrl))
+        else:
+            sys.exit('\n## Error : Error while getting %s\n' %(fetchUrl))
+
+        msgJson = json.loads(resPage.text)
+        pageSize = len(msgJson['data']['tracksAudioPlay']) 
+
+        for j in range(0, pageSize):
+            title = msgJson['data']['tracksAudioPlay'][j]['trackName'].replace(' ', '')
+            idx = int(msgJson['data']['tracksAudioPlay'][j]['index'])
+
+            index = idx #songNum - pageSize*i + idx - pageSize*(i-1)
+            m4aUrl = msgJson['data']['tracksAudioPlay'][j]['src']
+
+            prefix = "{:0>3}".format(str(index))+'_'
+            fileName = title+'.mp3'
+
+            if not noIndex:
+            	fileName = prefix+fileName
+
+            print('wget -O ', fileName, m4aUrl)
+            print('wget -O ', fileName, m4aUrl, file=f)
+
+
+    f.close()
+    p = subprocess.Popen(["chmod", "+x", outputFile], stdout=subprocess.PIPE)
+    print('\n## Info : save result to excutable file : ' + outputFile)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='download all audioes in ximalay album like :' + defaultAlbumUrl)
+    parser.add_argument('url', type=str, help="web url need to download")
+    parser.add_argument("-o", "--output", help="output file name")
+    parser.add_argument("-v", "--verbosity", help="increase output verbosity", action="store_true")
+    parser.add_argument("-n", "--noIndex", help="not add index to prefix", action="store_true")
+    
+
+    args = parser.parse_args()
+    webUrl = args.url
+    outputFile = args.output
+    Index = False
+
+    # input url format checker
+    if webUrl.find(urlModel0) == 0 or  webUrl.find(urlModel1) == 0:
+    	pass
+    else:
+    	sys.exit('\n ## Error : unrecognize url, please input correct url format like : ' + defaultAlbumUrl)
+
+    if args.output:
+    	outputFile = args.output
+    else:
+        outputFile = defaultOutputFile
+        print('## Info : save result to default File : ', defaultOutputFile)
+
+    if args.noIndex:
+    	print("## Info : don't add index to prefix")
+    	noIndex = True
+
+    main(webUrl, outputFile, noIndex)
